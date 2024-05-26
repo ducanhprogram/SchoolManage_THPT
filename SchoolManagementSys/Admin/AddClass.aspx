59a2a39733daf06dfd4758317261375ea49f977e
ducanhprogram <ducanh@DESKTOP-5C6E7RM>
2024-05-27 00:13:59 +0700

    Add project files.

---
+++ SchoolManagementSys/Admin/AddClass.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="AddClass.aspx.cs" Inherits="SchoolManagementSys.Admin.AddClass" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+     <style>
+         div {
+             color: #fff;
+         }
+       h3 {
+         color: #fff;
+        }
+
+       .table-container {
+            width: 100%;
+            overflow-x: auto;
+       }
+
+       .table {
+            width: 100%;
+            max-width: 100%;
+            background-color: #fff;
+            color: #000;
+         }
+
+    .table th, .table td {
+        padding: 15px;
+        text-align: left;
+    }
+
+    .table th {
+        background-color: #5558C9;
+        color: #fff;
+    }
+
+ </style>
+      <div style="background-image:url('../Image/pencil_artl.jpg'); width:100%; height:720px; background-repeat:no-repeat;background-size:cover;background-attachment:fixed">
+      <div class="container p-md-4 p-sm-4">
+          <div>
+              <asp:Label ID="lblMsg" runat="server"></asp:Label>
+          </div>
+          <h3 class="text-center">New Class</h3>
+
+        <%--margin-bottom--%>
+          <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+              <div class="col-md-6">
+                  <label style="font-size: 20px" for="txtClass">Class Name</label>
+                  <asp:TextBox ID="txtClass" runat="server" CssClass="form-control" placeholder="Enter Class Name" required ></asp:TextBox>
+              </div>
+          </div>
+
+          <div class="row mb-3 mr-lg-5 ml-lg-5">
+              <div class="col-md-3 col-md-offset-2 mb-3">
+                  <asp:Button ID="btnAdd" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9" Text="Add Class" OnClick="btnAdd_Click" />
+              </div>
+          </div>
+
+
+          <div class="row mb-3 mr-lg-5 ml-lg-5">     
+              <div class="col-md-6">
+                  <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-bordered" DataKeyNames="ClassId"
+                      OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" AutoGenerateColumns="False"
+                      EmptyDataText="No Record to display!" AllowPaging="true" PageSize="4" OnPageIndexChanging="GridView1_PageIndexChanging" >
+                      <Columns>
+                          <asp:BoundField DataField="Sr.No" HeaderText="Sr.No" ReadOnly="True">
+                              <ItemStyle HorizontalAlign="Center" />
+                          </asp:BoundField>
+                          <asp:TemplateField HeaderText="Class">
+                             
+                              <EditItemTemplate>
+                                  <asp:TextBox ID="txtClassEdit" runat="server" Text='<%# Eval("ClassName") %>' CssClass="form-control"></asp:TextBox>
+                              </EditItemTemplate>
+                              <ItemTemplate>
+                                   <asp:Label ID="lblClassName" runat="server" Text='<%# Eval("ClassName") %>'></asp:Label>
+                              </ItemTemplate>
+                               <ItemStyle HorizontalAlign="Center" />                                                   
+                             
+                          </asp:TemplateField>
+                          <asp:CommandField CausesValidation="False" ShowEditButton="True" HeaderText="Operation"></asp:CommandField>
+                      </Columns>
+
+                      <HeaderStyle BackColor="#5558C9"  ForeColor="White"/>
+
+                  </asp:GridView>
+              </div>
+          </div>
+      </div>
+  </div>
+</asp:Content>
---
+++ SchoolManagementSys/Admin/Subject.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSys.Models.SchoolCS;
+
+namespace SchoolManagementSys.Admin
+{
+    public partial class Subject : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+                GetSubject();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select * from Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            //ClaasId trong sql bảng clasa
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string classVal = ddlClass.SelectedItem.Text;
+                DataTable dt = fn.Fetch("Select *  from  Subject where ClassId = '" + ddlClass.SelectedItem.Value + "', SubjectName = '"+txtSubject.Text.Trim() + "')";
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Fees values('" + ddlClass.SelectedItem.Value + "' ,'" + txtSubject.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Succesffully!!!";
+                    lblMsg.CssClass = "alert alert-success";
+                    ddlClass.SelectedIndex = 0;
+                    txtSubject.Text = string.Empty;
+                    GetSubject();
+                }
+                else
+                {
+                    lblMsg.Text = "Inserted Subject already exists for <b>'" + classVal + "'</b> ";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+        }
+
+
+
+
+        private void GetSubject()
+        {
+            DataTable dt = fn.Fetch(@"Select Row_NUMBER() over(Order by (Select 1)) as [Sr.No], s.SubjectId, s.ClassId, c.ClassName, 
+                                    s.SubjectName from Subject s inner join Class c on c.ClassId = s.ClassId");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetSubject();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetSubject();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GetSubject();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int feesId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string feeAmt = (row.FindControl("TextBox1") as TextBox).Text;
+                fn.Query("Update Fees set FeesAmount = '" + feeAmt.Trim() + "' where FeesId = '" + feesId + "' ");
+                lblMsg.Text = "Fees Update Succesffully!!!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetSubject();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+    }
+}
